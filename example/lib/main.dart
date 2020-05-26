0c4e9c8ac986c5ce2413dbc94838b1ce8935574f
Enes Gunday <enes.gunday@commencis.com>
2020-05-26 18:27:38 +0300

    ExampleApp: Fix pixel overflow on landscape mode

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -55,177 +55,179 @@ class _HomePageState extends State<HomePage> {
         appBar: AppBar(
           title: Text('Fancy Dialog Example'),
         ),
-        body: Center(
-            child: Container(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            children: <Widget>[
-              AnimatedButton(
-                text: 'Issue Dialog',
-                pressEvent: () {
-                  showAlertDialogOnOkCallback(
-                      'Verified',
-                      'Sign In Success!, prees Ok to navigate.',
-                      DialogType.SUCCES,
-                      context,
-                      () => _slideMainPage(context));
-                },
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              AnimatedButton(
-                text: 'Info Dialog',
-                pressEvent: () {
-                  AwesomeDialog(
-                          context: context,
-                          headerAnimationLoop: false,
-                          dialogType: DialogType.INFO,
-                          animType: AnimType.BOTTOMSLIDE,
-                          tittle: 'INFO',
-                          desc:
-                              'Dialog description here..................................................',
-                          btnCancelOnPress: () {},
-                          btnOkOnPress: showDebugPrint)
-                      .show();
-                },
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              AnimatedButton(
-                text: 'Info Dialog Without buttons',
-                pressEvent: () {
-                  AwesomeDialog(
-                    context: context,
-                    headerAnimationLoop: false,
-                    dialogType: DialogType.INFO,
-                    animType: AnimType.BOTTOMSLIDE,
-                    tittle: 'INFO',
-                    desc: 'Dialog de.',
-                  ).show();
-                },
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              AnimatedButton(
-                text: 'Warning Dialog',
-                color: Colors.orange,
-                pressEvent: () {
-                  AwesomeDialog(
-                          context: context,
-                          dialogType: DialogType.WARNING,
-                          headerAnimationLoop: false,
-                          animType: AnimType.TOPSLIDE,
-                          tittle: 'Warning',
-                          desc:
-                              'Dialog description here..................................................',
-                          btnCancelOnPress: () {},
-                          btnOkOnPress: () {})
-                      .show();
-                },
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              AnimatedButton(
-                text: 'Error Dialog',
-                color: Colors.red,
-                textStyle: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 20),
-                pressEvent: () {
-                  AwesomeDialog(
-                          context: context,
-                          dialogType: DialogType.ERROR,
-                          animType: AnimType.RIGHSLIDE,
-                          headerAnimationLoop: false,
-                          tittle: 'Error',
-                          desc:
-                              'Dialog description here..................................................',
-                          btnOkOnPress: () {},
-                          btnOkColor: Colors.red)
-                      .show();
-                },
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              AnimatedButton(
-                text: 'Succes Dialog',
-                color: Colors.green,
-                pressEvent: () {
-                  AwesomeDialog(
+        body: SingleChildScrollView(
+          child: Center(
+              child: Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: <Widget>[
+                AnimatedButton(
+                  text: 'Issue Dialog',
+                  pressEvent: () {
+                    showAlertDialogOnOkCallback(
+                        'Verified',
+                        'Sign In Success!, prees Ok to navigate.',
+                        DialogType.SUCCES,
+                        context,
+                        () => _slideMainPage(context));
+                  },
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                AnimatedButton(
+                  text: 'Info Dialog',
+                  pressEvent: () {
+                    AwesomeDialog(
+                            context: context,
+                            headerAnimationLoop: false,
+                            dialogType: DialogType.INFO,
+                            animType: AnimType.BOTTOMSLIDE,
+                            tittle: 'INFO',
+                            desc:
+                                'Dialog description here..................................................',
+                            btnCancelOnPress: () {},
+                            btnOkOnPress: showDebugPrint)
+                        .show();
+                  },
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                AnimatedButton(
+                  text: 'Info Dialog Without buttons',
+                  pressEvent: () {
+                    AwesomeDialog(
                       context: context,
-                      animType: AnimType.LEFTSLIDE,
                       headerAnimationLoop: false,
-                      dialogType: DialogType.SUCCES,
-                      tittle: 'Succes',
-                      desc:
-                          'Dialog description here..................................................',
-                      btnOkOnPress: () {
-                        debugPrint('OnClcik');
-                      },
-                      btnOkIcon: Icons.check_circle,
-                      onDissmissCallback: () {
-                        debugPrint('Dialog Dissmiss from callback');
-                      }).show();
-                },
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              AnimatedButton(
-                text: 'Custom Body Dialog',
-                color: Colors.blueGrey,
-                pressEvent: () {
-                  AwesomeDialog(
-                    context: context,
-                    animType: AnimType.SCALE,
-                    dialogType: DialogType.INFO,
-                    body: Center(
-                      child: Text(
-                        'If the body is specified, then title and description will be ignored, this allows to further customize the dialogue.',
-                        style: TextStyle(fontStyle: FontStyle.italic),
+                      dialogType: DialogType.INFO,
+                      animType: AnimType.BOTTOMSLIDE,
+                      tittle: 'INFO',
+                      desc: 'Dialog de.',
+                    ).show();
+                  },
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                AnimatedButton(
+                  text: 'Warning Dialog',
+                  color: Colors.orange,
+                  pressEvent: () {
+                    AwesomeDialog(
+                            context: context,
+                            dialogType: DialogType.WARNING,
+                            headerAnimationLoop: false,
+                            animType: AnimType.TOPSLIDE,
+                            tittle: 'Warning',
+                            desc:
+                                'Dialog description here..................................................',
+                            btnCancelOnPress: () {},
+                            btnOkOnPress: () {})
+                        .show();
+                  },
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                AnimatedButton(
+                  text: 'Error Dialog',
+                  color: Colors.red,
+                  textStyle: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 20),
+                  pressEvent: () {
+                    AwesomeDialog(
+                            context: context,
+                            dialogType: DialogType.ERROR,
+                            animType: AnimType.RIGHSLIDE,
+                            headerAnimationLoop: false,
+                            tittle: 'Error',
+                            desc:
+                                'Dialog description here..................................................',
+                            btnOkOnPress: () {},
+                            btnOkColor: Colors.red)
+                        .show();
+                  },
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                AnimatedButton(
+                  text: 'Succes Dialog',
+                  color: Colors.green,
+                  pressEvent: () {
+                    AwesomeDialog(
+                        context: context,
+                        animType: AnimType.LEFTSLIDE,
+                        headerAnimationLoop: false,
+                        dialogType: DialogType.SUCCES,
+                        tittle: 'Succes',
+                        desc:
+                            'Dialog description here..................................................',
+                        btnOkOnPress: () {
+                          debugPrint('OnClcik');
+                        },
+                        btnOkIcon: Icons.check_circle,
+                        onDissmissCallback: () {
+                          debugPrint('Dialog Dissmiss from callback');
+                        }).show();
+                  },
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                AnimatedButton(
+                  text: 'Custom Body Dialog',
+                  color: Colors.blueGrey,
+                  pressEvent: () {
+                    AwesomeDialog(
+                      context: context,
+                      animType: AnimType.SCALE,
+                      dialogType: DialogType.INFO,
+                      body: Center(
+                        child: Text(
+                          'If the body is specified, then title and description will be ignored, this allows to further customize the dialogue.',
+                          style: TextStyle(fontStyle: FontStyle.italic),
+                        ),
                       ),
-                    ),
-                    tittle: 'This is Ignored',
-                    desc: 'This is also Ignored',
-                  ).show();
-                },
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              AnimatedButton(
-                text: 'Custom Buttons Dialog',
-                color: Colors.brown,
-                pressEvent: () {
-                  AwesomeDialog(
-                    context: context,
-                    animType: AnimType.SCALE,
-                    customHeader: Icon(
-                      Icons.face,
-                      size: 50,
-                    ),
+                      tittle: 'This is Ignored',
+                      desc: 'This is also Ignored',
+                    ).show();
+                  },
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                AnimatedButton(
+                  text: 'Custom Buttons Dialog',
+                  color: Colors.brown,
+                  pressEvent: () {
+                    AwesomeDialog(
+                      context: context,
+                      animType: AnimType.SCALE,
+                      customHeader: Icon(
+                        Icons.face,
+                        size: 50,
+                      ),
 
-                    tittle: 'This is Custom',
-                    desc: 'This is custom button and header',
-                    btnOk: FlatButton(
-                      child: Text('Custom Button'),
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                    //this is ignored
-                    btnOkOnPress: () {},
-                  ).show();
-                },
-              ),
-            ],
-          ),
-        )));
+                      tittle: 'This is Custom',
+                      desc: 'This is custom button and header',
+                      btnOk: FlatButton(
+                        child: Text('Custom Button'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      //this is ignored
+                      btnOkOnPress: () {},
+                    ).show();
+                  },
+                ),
+              ],
+            ),
+          )),
+        ));
   }
 }
